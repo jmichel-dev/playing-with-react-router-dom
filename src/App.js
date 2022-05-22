31af3d52ad97968459a3742e52db05fdbe8462c4
Jean Michel <jeaanmichel@protonmail.com>
2022-05-22 17:35:53 -0300

    added routes for product details

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import { Route } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./page/ProductDetail";
 
 import "./index.css";
 import Home from "./page/Home";
 import Products from "./page/Products";
+import { Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+          <Route exact path="/products">
+            <Products />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/page/Products.js
+++ src/page/Products.js
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-  return <h1>Products page</h1>;
+  return (
+    <section>
+      <h1>Products page</h1>
+      <ul>
+        <Link to="/products/p1">A book</Link>
+        <Link to="/products/p2">A carpet</Link>
+        <Link to="/products/p3">An online course</Link>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
